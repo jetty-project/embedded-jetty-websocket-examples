2688ecad5375c4eae59c1424c3f119d9f620b31d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-12-01 07:25:54 -0700

    Example of Native Jetty WebSocket API

---
+++ native-jetty-websocket-example/src/main/java/org/eclipse/jetty/demo/EventClient.java
@@ -0,0 +1,42 @@
+package org.eclipse.jetty.demo;
+
+import java.net.URI;
+import java.util.concurrent.Future;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+
+public class EventClient
+{
+    public static void main(String[] args)
+    {
+        URI uri = URI.create("ws://localhost:8080/events/");
+
+        WebSocketClient client = new WebSocketClient();
+        try
+        {
+            try
+            {
+                client.start();
+                // The socket that receives events
+                EventSocket socket = new EventSocket();
+                // Attempt Connect
+                Future<Session> fut = client.connect(socket,uri);
+                // Wait for Connect
+                Session session = fut.get();
+                // Send a message
+                session.getRemote().sendString("Hello");
+                // Close session
+                session.close();
+            }
+            finally
+            {
+                client.stop();
+            }
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace(System.err);
+        }
+    }
+}
---
+++ native-jetty-websocket-example/src/main/java/org/eclipse/jetty/demo/EventSocket.java
@@ -0,0 +1,35 @@
+package org.eclipse.jetty.demo;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+
+public class EventSocket extends WebSocketAdapter
+{
+    @Override
+    public void onWebSocketConnect(Session sess)
+    {
+        super.onWebSocketConnect(sess);
+        System.out.println("Socket Connected: " + sess);
+    }
+    
+    @Override
+    public void onWebSocketText(String message)
+    {
+        super.onWebSocketText(message);
+        System.out.println("Received TEXT message: " + message);
+    }
+    
+    @Override
+    public void onWebSocketClose(int statusCode, String reason)
+    {
+        super.onWebSocketClose(statusCode,reason);
+        System.out.println("Socket Closed: [" + statusCode + "] " + reason);
+    }
+    
+    @Override
+    public void onWebSocketError(Throwable cause)
+    {
+        super.onWebSocketError(cause);
+        cause.printStackTrace(System.err);
+    }
+}
