b7f70ce946389f7540bf7c58f592571cac4df084
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-07-13 15:03:31 -0500

    Experiment to attempt to replicate https://github.com/eclipse/jetty.project/issues/8295
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- native-jetty-websocket-example/src/main/java/org/eclipse/jetty/demo/EventClient.java
+++ native-jetty-websocket-example/src/main/java/org/eclipse/jetty/demo/EventClient.java
@@ -19,27 +19,46 @@
 package org.eclipse.jetty.demo;
 
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpClientTransport;
+import org.eclipse.jetty.client.http.HttpClientTransportOverHTTP;
+import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 
 public class EventClient
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         URI uri = URI.create("ws://localhost:8080/events/");
 
-        WebSocketClient client = new WebSocketClient();
+        ExecutorService executor = Executors.newFixedThreadPool(8, r -> {
+            Thread thread = new Thread(r);
+            thread.setName("EventClientThread-");
+            return thread;
+        });
+
+        int selectorCount = 1;
+        HttpClientTransportOverHTTP transport = new HttpClientTransportOverHTTP(selectorCount);
+        HttpClient httpClient = new HttpClient(transport);
+        httpClient.setExecutor(executor);
         try
         {
+            httpClient.start();
+
+            WebSocketClient wsClient = new WebSocketClient(httpClient);
             try
             {
-                client.start();
+                wsClient.start();
                 // The socket that receives events
                 EventSocket socket = new EventSocket();
                 // Attempt Connect
-                Future<Session> fut = client.connect(socket,uri);
+                Future<Session> fut = wsClient.connect(socket,uri);
                 // Wait for Connect
                 Session session = fut.get();
 
@@ -57,12 +76,17 @@ public class EventClient
             }
             finally
             {
-                client.stop();
+                LifeCycle.stop(wsClient);
             }
         }
         catch (Throwable t)
         {
             t.printStackTrace(System.err);
         }
+        finally
+        {
+            LifeCycle.stop(httpClient);
+        }
+        System.exit(0);
     }
 }
